1a7e560362e614535ee984d68f3b48701e172b70
jonathan <jonedmen@espol.edu.ec>
2013-11-02 21:21:01 -0500

    Implementacion buscaminas en Java

---
+++ Java/Buscaminas/src/lenguajes/main.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lenguajes;
+
+/**
+ *
+ * @author jonathan
+ */
+public class main {
+    static Tablero tablero = new Tablero("dificil");
+    public static void main(String []args){
+    }
+}
